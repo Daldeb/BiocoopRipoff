3c9efb44d0957042e748ad8655e4c9cf70e37f6f
daldeb <daldeb.pro@gmail.com>
2023-05-15 04:58:01 +0200

    first

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les Cinq Legumes</title>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="/Images/Logos/LogoMinimal.png" alt="Logo">
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Rechercher un produit ...">
+            <img src="/Images/Logos/loupe.svg" alt="Search">
+        </div>
+        <div class="about">
+            <img src="/Images/Logos/logotext.png" alt="LogoTextOnly">
+            <img src="/Images/Logos/help.svg" alt="About">
+        </div>
+        <div class="user">
+            <img src="/Images/Logos/user.svg" alt="User">
+            <a href="/user.php">Mon compte</a>
+        </div>
+        <div class="cart">
+            <img src="/Images/Logos/basket.svg" alt="Cart">
+            <span>Panier</span>
+        </div>
+    </header>
+    <!-- Your HTML and PHP code goes here -->
+
+    <script src="js/script.js"></script>
+</body>
+</html>
+
